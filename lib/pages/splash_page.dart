18c84b905a415da6582ec680e75657052f2f74c9
soulofjava <isamaulanatantra@gmail.com>
2022-07-18 13:19:50 +0700

    splash screen #1

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff4141A4),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Image.asset('assets/logo.png'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
